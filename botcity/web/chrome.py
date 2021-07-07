b8498150ac1adb79feafe5c4efa6c984cf3ef038
Hugo Slepicka <hhslepicka@gmail.com>
2021-07-06 19:34:17 -0700

    FIX: Add additional Chrome path for Windows.

--- botcity/web/chrome.py
+++ botcity/web/chrome.py
@@ -15,7 +15,8 @@ CHROME_BINARIES = [
     "/Applications/Chromium.app/Contents/MacOS/Chromium",
     "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
     "/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary",
-    "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe"
+    "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe",
+    "C:/Program Files/Google/Chrome/Application/chrome.exe"
 ]
 
 

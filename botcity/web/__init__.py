7f2838b2f70741164ef473d7df60e6f74eaadca0
kayqueGovetri <govetriKayque@gmail.com>
2022-12-01 16:09:49 -0300

    STY: Ajust in __init__

--- botcity/web/__init__.py
+++ botcity/web/__init__.py
@@ -1,11 +1,11 @@
-### PATCH Selenium for Compat ###
-from .compat import patch_selenium
-patch_selenium()
+# PATCH Selenium for compat
+from .compat import patch_selenium  # noqa: F401, F403, E402
+patch_selenium()  # noqa: F401, F403, E402
 
-from .bot import WebBot, Browser, BROWSER_CONFIGS, By, PageLoadStrategy  # noqa: F401, F403
-from .parsers import table_to_dict, data_from_row, sanitize_header  # noqa: F401, F403
-from .util import element_as_select  # noqa: F401, F403
+from .bot import WebBot, Browser, BROWSER_CONFIGS, By, PageLoadStrategy  # noqa: F401, F403, E402
+from .parsers import table_to_dict, data_from_row, sanitize_header  # noqa: F401, F403, E402
+from .util import element_as_select  # noqa: F401, F403, E402
 
-from botcity.web._version import get_versions
+from botcity.web._version import get_versions  # noqa: F401, F403, E402
 __version__ = get_versions()['version']
 del get_versions

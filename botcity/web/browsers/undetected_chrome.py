f05b0e11bc08fe17a0cc68a3cc3175e3ff04b312
kayqueGovetri <govetriKayque@gmail.com>
2023-03-09 22:05:34 -0300

    STY: Ajust linter

--- botcity/web/browsers/undetected_chrome.py
+++ botcity/web/browsers/undetected_chrome.py
@@ -9,9 +9,8 @@ from undetected_chromedriver.options import ChromeOptions
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from ..util import cleanup_temp_dir
 
-
 try:
-    from undetected_chromedriver import Service as ChromeService # noqa: F401, F403
+    from undetected_chromedriver import Service as ChromeService  # noqa: F401, F403
 except ImportError:
     from undetected_chromedriver import Chrome as ChromeService  # noqa: F401, F403
 

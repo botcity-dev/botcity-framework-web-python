0c797a565fbd7cbd4420905b1d09399a5e34297c
Hugo Slepicka <hhslepicka@gmail.com>
2023-03-09 21:01:39 -0800

    FIX: Add new headless parameter for Chrome > 108.

--- botcity/web/browsers/chrome.py
+++ botcity/web/browsers/chrome.py
@@ -57,6 +57,7 @@ def default_options(headless=False, download_folder_path=None, user_data_dir=Non
 
     if headless:
         chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--headless=new")
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--hide-scrollbars")
         chrome_options.add_argument("--mute-audio")
